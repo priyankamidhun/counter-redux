2032449c7e5b83671b231e099fe5827a8a07a348
priyankamidhun <priyankamidhun123@gmail.com>
2023-12-23 10:37:05 +0530

    counter app using redux

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,28 @@
+// here we define action and reducer
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+    name: 'counterApp',
+    initialState: {
+        value: 0
+    },
+    reducers: {
+        // actions are created inside this reducers key.(actions are logics)
+        // logic to update the data
+        increment: (state, action) => {
+            state.value = state.value + action.payload;
+        },
+        decrement: (state, action) => {
+            console.log("===Action===");
+            console.log(action);
+            state.value = state.value - action.payload;
+        },
+        reset: (state) => {
+            state.value = 0;
+        }
+    }
+})
+
+export const { increment, decrement, reset } = counterSlice.actions;
+export default counterSlice.reducer;
